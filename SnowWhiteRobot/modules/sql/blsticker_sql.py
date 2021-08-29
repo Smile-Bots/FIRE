59ced4a48a9189d6c7ac1fe4f942ad0234844ad9
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-08-29 14:09:56 +0530

    Update blsticker_sql.py

--- SnowWhiteRobot/modules/sql/blsticker_sql.py
+++ SnowWhiteRobot/modules/sql/blsticker_sql.py
@@ -1,11 +1,12 @@
 import threading
 
-from SnowWhiteRobot.modules.sql import BASE, SESSION
-from sqlalchemy import Column, Integer, String, UnicodeText, distinct, func
+from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
+
+from SnowWhiteRobot.modules.sql import SESSION, BASE
 
 
-class StickersFilters(BASE):
-    __tablename__ = "blacklist_stickers"
+class BlackListFilters(BASE):
+    __tablename__ = "blacklist"
     chat_id = Column(String(14), primary_key=True)
     trigger = Column(UnicodeText, primary_key=True, nullable=False)
 
@@ -14,16 +15,18 @@ class StickersFilters(BASE):
         self.trigger = trigger
 
     def __repr__(self):
-        return "<Stickers filter '%s' for %s>" % (self.trigger, self.chat_id)
+        return "<Blacklist filter '%s' for %s>" % (self.trigger, self.chat_id)
 
     def __eq__(self, other):
         return bool(
-            isinstance(other, StickersFilters) and
-            self.chat_id == other.chat_id and self.trigger == other.trigger)
+            isinstance(other, BlackListFilters)
+            and self.chat_id == other.chat_id
+            and self.trigger == other.trigger
+        )
 
 
-class StickerSettings(BASE):
-    __tablename__ = "blsticker_settings"
+class BlacklistSettings(BASE):
+    __tablename__ = "blacklist_settings"
     chat_id = Column(String(14), primary_key=True)
     blacklist_type = Column(Integer, default=1)
     value = Column(UnicodeText, default="0")
@@ -35,42 +38,41 @@ class StickerSettings(BASE):
 
     def __repr__(self):
         return "<{} will executing {} for blacklist trigger.>".format(
-            self.chat_id, self.blacklist_type)
+            self.chat_id, self.blacklist_type
+        )
 
 
-StickersFilters.__table__.create(checkfirst=True)
-StickerSettings.__table__.create(checkfirst=True)
+BlackListFilters.__table__.create(checkfirst=True)
+BlacklistSettings.__table__.create(checkfirst=True)
 
-STICKERS_FILTER_INSERTION_LOCK = threading.RLock()
-STICKSET_FILTER_INSERTION_LOCK = threading.RLock()
+BLACKLIST_FILTER_INSERTION_LOCK = threading.RLock()
+BLACKLIST_SETTINGS_INSERTION_LOCK = threading.RLock()
 
-CHAT_STICKERS = {}
-CHAT_BLSTICK_BLACKLISTS = {}
+CHAT_BLACKLISTS = {}
+CHAT_SETTINGS_BLACKLISTS = {}
 
 
-def add_to_stickers(chat_id, trigger):
-    with STICKERS_FILTER_INSERTION_LOCK:
-        stickers_filt = StickersFilters(str(chat_id), trigger)
+def add_to_blacklist(chat_id, trigger):
+    with BLACKLIST_FILTER_INSERTION_LOCK:
+        blacklist_filt = BlackListFilters(str(chat_id), trigger)
 
-        SESSION.merge(stickers_filt)  # merge to avoid duplicate key issues
+        SESSION.merge(blacklist_filt)  # merge to avoid duplicate key issues
         SESSION.commit()
-        global CHAT_STICKERS
-        if CHAT_STICKERS.get(str(chat_id), set()) == set():
-            CHAT_STICKERS[str(chat_id)] = {trigger}
+        global CHAT_BLACKLISTS
+        if CHAT_BLACKLISTS.get(str(chat_id), set()) == set():
+            CHAT_BLACKLISTS[str(chat_id)] = {trigger}
         else:
-            CHAT_STICKERS.get(str(chat_id), set()).add(trigger)
+            CHAT_BLACKLISTS.get(str(chat_id), set()).add(trigger)
 
 
-def rm_from_stickers(chat_id, trigger):
-    with STICKERS_FILTER_INSERTION_LOCK:
-        stickers_filt = SESSION.query(StickersFilters).get(
-            (str(chat_id), trigger))
-        if stickers_filt:
-            if trigger in CHAT_STICKERS.get(str(chat_id),
-                                            set()):  # sanity check
-                CHAT_STICKERS.get(str(chat_id), set()).remove(trigger)
+def rm_from_blacklist(chat_id, trigger):
+    with BLACKLIST_FILTER_INSERTION_LOCK:
+        blacklist_filt = SESSION.query(BlackListFilters).get((str(chat_id), trigger))
+        if blacklist_filt:
+            if trigger in CHAT_BLACKLISTS.get(str(chat_id), set()):  # sanity check
+                CHAT_BLACKLISTS.get(str(chat_id), set()).remove(trigger)
 
-            SESSION.delete(stickers_filt)
+            SESSION.delete(blacklist_filt)
             SESSION.commit()
             return True
 
@@ -78,29 +80,31 @@ def rm_from_stickers(chat_id, trigger):
         return False
 
 
-def get_chat_stickers(chat_id):
-    return CHAT_STICKERS.get(str(chat_id), set())
+def get_chat_blacklist(chat_id):
+    return CHAT_BLACKLISTS.get(str(chat_id), set())
 
 
-def num_stickers_filters():
+def num_blacklist_filters():
     try:
-        return SESSION.query(StickersFilters).count()
+        return SESSION.query(BlackListFilters).count()
     finally:
         SESSION.close()
 
 
-def num_stickers_chat_filters(chat_id):
+def num_blacklist_chat_filters(chat_id):
     try:
-        return SESSION.query(StickersFilters.chat_id).filter(
-            StickersFilters.chat_id == str(chat_id)).count()
+        return (
+            SESSION.query(BlackListFilters.chat_id)
+            .filter(BlackListFilters.chat_id == str(chat_id))
+            .count()
+        )
     finally:
         SESSION.close()
 
 
-def num_stickers_filter_chats():
+def num_blacklist_filter_chats():
     try:
-        return SESSION.query(func.count(distinct(
-            StickersFilters.chat_id))).scalar()
+        return SESSION.query(func.count(distinct(BlackListFilters.chat_id))).scalar()
     finally:
         SESSION.close()
 
@@ -115,18 +119,19 @@ def set_blacklist_strength(chat_id, blacklist_type, value):
     # 5 = ban
     # 6 = tban
     # 7 = tmute
-    with STICKSET_FILTER_INSERTION_LOCK:
-        global CHAT_BLSTICK_BLACKLISTS
-        curr_setting = SESSION.query(StickerSettings).get(str(chat_id))
+    with BLACKLIST_SETTINGS_INSERTION_LOCK:
+        global CHAT_SETTINGS_BLACKLISTS
+        curr_setting = SESSION.query(BlacklistSettings).get(str(chat_id))
         if not curr_setting:
-            curr_setting = StickerSettings(
-                chat_id, blacklist_type=int(blacklist_type), value=value)
+            curr_setting = BlacklistSettings(
+                chat_id, blacklist_type=int(blacklist_type), value=value
+            )
 
         curr_setting.blacklist_type = int(blacklist_type)
         curr_setting.value = str(value)
-        CHAT_BLSTICK_BLACKLISTS[str(chat_id)] = {
-            'blacklist_type': int(blacklist_type),
-            'value': value
+        CHAT_SETTINGS_BLACKLISTS[str(chat_id)] = {
+            "blacklist_type": int(blacklist_type),
+            "value": value,
         }
 
         SESSION.add(curr_setting)
@@ -135,9 +140,9 @@ def set_blacklist_strength(chat_id, blacklist_type, value):
 
 def get_blacklist_setting(chat_id):
     try:
-        setting = CHAT_BLSTICK_BLACKLISTS.get(str(chat_id))
+        setting = CHAT_SETTINGS_BLACKLISTS.get(str(chat_id))
         if setting:
-            return setting['blacklist_type'], setting['value']
+            return setting["blacklist_type"], setting["value"]
         else:
             return 1, "0"
 
@@ -145,31 +150,31 @@ def get_blacklist_setting(chat_id):
         SESSION.close()
 
 
-def __load_CHAT_STICKERS():
-    global CHAT_STICKERS
+def __load_chat_blacklists():
+    global CHAT_BLACKLISTS
     try:
-        chats = SESSION.query(StickersFilters.chat_id).distinct().all()
+        chats = SESSION.query(BlackListFilters.chat_id).distinct().all()
         for (chat_id,) in chats:  # remove tuple by ( ,)
-            CHAT_STICKERS[chat_id] = []
+            CHAT_BLACKLISTS[chat_id] = []
 
-        all_filters = SESSION.query(StickersFilters).all()
+        all_filters = SESSION.query(BlackListFilters).all()
         for x in all_filters:
-            CHAT_STICKERS[x.chat_id] += [x.trigger]
+            CHAT_BLACKLISTS[x.chat_id] += [x.trigger]
 
-        CHAT_STICKERS = {x: set(y) for x, y in CHAT_STICKERS.items()}
+        CHAT_BLACKLISTS = {x: set(y) for x, y in CHAT_BLACKLISTS.items()}
 
     finally:
         SESSION.close()
 
 
-def __load_chat_stickerset_blacklists():
-    global CHAT_BLSTICK_BLACKLISTS
+def __load_chat_settings_blacklists():
+    global CHAT_SETTINGS_BLACKLISTS
     try:
-        chats_settings = SESSION.query(StickerSettings).all()
+        chats_settings = SESSION.query(BlacklistSettings).all()
         for x in chats_settings:  # remove tuple by ( ,)
-            CHAT_BLSTICK_BLACKLISTS[x.chat_id] = {
-                'blacklist_type': x.blacklist_type,
-                'value': x.value
+            CHAT_SETTINGS_BLACKLISTS[x.chat_id] = {
+                "blacklist_type": x.blacklist_type,
+                "value": x.value,
             }
 
     finally:
@@ -177,14 +182,16 @@ def __load_chat_stickerset_blacklists():
 
 
 def migrate_chat(old_chat_id, new_chat_id):
-    with STICKERS_FILTER_INSERTION_LOCK:
-        chat_filters = SESSION.query(StickersFilters).filter(
-            StickersFilters.chat_id == str(old_chat_id)).all()
+    with BLACKLIST_FILTER_INSERTION_LOCK:
+        chat_filters = (
+            SESSION.query(BlackListFilters)
+            .filter(BlackListFilters.chat_id == str(old_chat_id))
+            .all()
+        )
         for filt in chat_filters:
             filt.chat_id = str(new_chat_id)
         SESSION.commit()
 
 
-__load_CHAT_STICKERS()
-__load_chat_stickerset_blacklists()
-
+__load_chat_blacklists()
+__load_chat_settings_blacklists()
